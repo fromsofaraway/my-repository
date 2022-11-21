7eea7ba026ad2f1cefcac87890ae8ad7594c4c16
fromsofaraway <egoris.effer@yandex.ru>
2022-11-21 19:31:08 +0600

    переделал daoв трай с ресурсами

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,13 +7,15 @@ public class Main {
     public static void main(String[] args) {
         UserService users = new UserServiceImpl();
         users.createUsersTable();
-        users.saveUser("Egor", "Brows", (byte) 24);
-        users.saveUser("Sven", "Cat", (byte) 3);
-        users.saveUser("Daria", "Ice", (byte) 19);
-        users.saveUser("Max", "Cat", (byte) 7);
+        users.saveUser("Egor", "Brows", (byte) 25);
+        users.saveUser("Sven", "Cat", (byte) 4);
+        users.saveUser("Daria", "Ice", (byte) 20);
+        users.saveUser("Max", "Cat", (byte) 11);
         System.out.println(users.getAllUsers());
         users.removeUserById(1);
         users.removeUserById(4);
+        System.out.println(users.getAllUsers());
+        users.saveUser("Crispy", "Cat", (byte) 1);
         System.out.println(users.getAllUsers());
         users.cleanUsersTable();
         users.dropUsersTable();
